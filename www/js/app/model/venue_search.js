9f8b872ad00d6bb0ca75d46b87c10b8f9a1a2359
Ben Glassman <ben@vtdesignworks.com>
2013-06-06 00:56:30 -0400

    Add defaults to venue search model.

--- www/js/app/model/venue_search.js
+++ www/js/app/model/venue_search.js
@@ -1,5 +1,9 @@
 define(['backbone'], function(Backbone) {
-	var VenueSearch = Backbone.Model.extend();
+	var VenueSearch = Backbone.Model.extend({
+		defaults: {
+			location: ''
+		}
+	});
 
 	return VenueSearch;
 });
