5d1a27d6e05eee3ac963cae08b7839d9022bdb5c
Ben Glassman <ben@vtdesignworks.com>
2013-06-05 23:19:51 -0400

    Remove cruft from router.

--- www/js/app/router.js
+++ www/js/app/router.js
@@ -2,18 +2,10 @@ define(['backbone'], function(Backbone) {
 
 	return new (Backbone.Router.extend({
 		routes: {
-			"":"index",
-			"venue/:slug":"venueDetail",
-			"list/foo":"listFoo"
+			"":"index"
 		},
 		start: function () {
 			Backbone.history.start({ pushState: true });
-		},
-		index: function () {
-			console.log('Router.index, go!');
-		},
-		listFoo: function () {
-			console.log('list the foo');
 		}
 	}));
 
