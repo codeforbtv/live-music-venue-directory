f0f2c5d7915b9026ef7f2e1b032afc6058de0627
Ben Glassman <ben@vtdesignworks.com>
2013-06-06 23:24:42 -0400

    Move ajax start/end event listening to AppView. Tabs to spaces.

--- www/js/app.js
+++ www/js/app.js
@@ -1,34 +1,20 @@
 define(
-['jquery', 'router', 'app/event_dispatcher', 'view/app'], 
+['jquery', 'router', 'app/event_dispatcher', 'view/app'],
 function($, router, dispatcher, AppView) {
 
-	var App = function() {
-		this.router = router;
-		this.dispatcher = dispatcher;
-	};
+    var App = function() {
+        this.router = router;
+        this.dispatcher = dispatcher;
+    };
 
-	App.prototype.events = {
-		'search': 'search'
-	};
+    App.prototype.init = function(options) {
+        this.options = options;
+        this.appView = new AppView({
+            el: $('#app')
+        });
+        this.router.start();
+    };
 
-	App.prototype.init = function(options) {
-		this.options = options;
-		this.setupEvents();
-		this.appView = new AppView({
-			el: $('#app')
-		});
-		this.router.start();
-	};
-
-	App.prototype.setupEvents = function() {
-		for (x in this.events) {
-			var callback = this.events[x];
-			this.dispatcher.on(x, this[callback], this);
-		}
-		$(document).ajaxStart(function() { dispatcher.trigger('ajax.start'); });
-		$(document).ajaxComplete(function() { dispatcher.trigger('ajax.complete'); });
-	};
-
-	return new App();
+    return new App();
 
 });
--- www/js/app/view/app.js
+++ www/js/app/view/app.js
@@ -2,59 +2,61 @@ define(
 ['underscore', 'backbone', 'app/event_dispatcher', 'text!tpl/app.html', 'view/venue_search_form', 'collection/venue', 'view/venue_collection_list', 'view/venue_collection_map'],
 function(_, Backbone, dispatcher, tpl, VenueSearchFormView, VenueCollection, VenueCollectionListView, VenueCollectionMapView) {
 
-	var AppView = Backbone.View.extend({
-		template: _.template(tpl),
-		initialize: function(venue) {
-			this.render();
-			this.loadingMessage = this.$('#loading').hide();
-			dispatcher.on('search', this.search, this);
-			dispatcher.on('ajax.start', this.ajaxStart, this);
-			dispatcher.on('ajax.complete', this.ajaxComplete, this);
-		},
-		render: function() {
-			this.$el.html(this.template());
-			this.searchView = new VenueSearchFormView({
-				el: this.$('#search-form')
-			});
-			this.mapView = new VenueCollectionMapView({
-				el: this.$('#search-map')
-			});
-			this.listView = new VenueCollectionListView({
-				el: this.$('#search-list')
-			});
-		}
-	});
+    var AppView = Backbone.View.extend({
+        template: _.template(tpl),
+        initialize: function(venue) {
+            this.render();
+            this.loadingMessage = this.$('#loading').hide();
+            dispatcher.on('search', this.search, this);
+            dispatcher.on('ajax.start', this.ajaxStart, this);
+            dispatcher.on('ajax.complete', this.ajaxComplete, this);
+            $(document).ajaxStart(function() { dispatcher.trigger('ajax.start'); });
+            $(document).ajaxComplete(function() { dispatcher.trigger('ajax.complete'); });
+        },
+        render: function() {
+            this.$el.html(this.template());
+            this.searchView = new VenueSearchFormView({
+                el: this.$('#search-form')
+            });
+            this.mapView = new VenueCollectionMapView({
+                el: this.$('#search-map')
+            });
+            this.listView = new VenueCollectionListView({
+                el: this.$('#search-list')
+            });
+        }
+    });
 
-	AppView.prototype.search = function(criteria) {
-		var _this = this;
-		var venueCollection = new VenueCollection();
-		venueCollection.fetch({
-			data: criteria,
-			success: function(collection) {
-				_this.searchComplete(collection);
-			}
-		});
-	};
+    AppView.prototype.search = function(criteria) {
+        var _this = this;
+        var venueCollection = new VenueCollection();
+        venueCollection.fetch({
+            data: criteria,
+            success: function(collection) {
+                _this.searchComplete(collection);
+            }
+        });
+    };
 
-	AppView.prototype.searchComplete = function(collection) {
-		if (collection.length) {
-			this.$el.addClass('twocol');
-			this.listView.collection = collection;
-			this.listView.render();
-			this.mapView.collection = collection;
-			this.mapView.render();
-		} else {
-			alert('No venues match your search');
-		}
-	};
+    AppView.prototype.searchComplete = function(collection) {
+        if (collection.length) {
+            this.$el.addClass('twocol');
+            this.listView.collection = collection;
+            this.listView.render();
+            this.mapView.collection = collection;
+            this.mapView.render();
+        } else {
+            alert('No venues match your search');
+        }
+    };
 
-	AppView.prototype.ajaxStart = function() {
-		this.loadingMessage.show();
-	};
+    AppView.prototype.ajaxStart = function() {
+        this.loadingMessage.show();
+    };
 
-	AppView.prototype.ajaxComplete = function() {
-		this.loadingMessage.hide();
-	};
+    AppView.prototype.ajaxComplete = function() {
+        this.loadingMessage.hide();
+    };
 
-	return AppView;
+    return AppView;
 });
